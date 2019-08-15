6f644e29a6bd7ebfdc88f0a277ddb184a7fa8cb8
paopaol <467847281@qq.com>
2019-08-15 21:36:15 +0800

    fix some bug on Dijkstra algorithm

--- include/algorithm/Dijkstra.h
+++ include/algorithm/Dijkstra.h
@@ -3,67 +3,94 @@
 
 #include <assert.h>
 #include <map>
+#include <memory>
 #include <string>
 #include <vector>
 
 namespace algorithm {
 using TimeSecond = unsigned long long;
-const TimeSecond maxTimeSecond = 0xffffffff;
 using NodeName = std::string;
 
-class Node;
-using Neighbors = std::map<NodeName, Node>;
+const TimeSecond maxTimeSecond = 0xffffffff;
 
-class Node {
+class Node;
+using NodePtr = std::shared_ptr<Node>;
+using Neighbors = std::map<NodeName, NodePtr>;
+using NodeToNodeTimeSecondMap = std::map<NodeName, TimeSecond>;
+
+class Node : public std::enable_shared_from_this<Node> {
 public:
   Node() {}
-  Node(const NodeName name, TimeSecond overhead)
-      : name_(name), overhead_(overhead) {}
+  Node(const NodeName &name) : name_(name) {}
   ~Node() {}
-  Node &addNeighbor(const NodeName &name, TimeSecond overhead) {
-    Node node(name, overhead);
-
-    neighbors_[name] = node;
-    return neighbors_[name];
+  void connect(const NodePtr &toNode, TimeSecond overhead) {
+    // toNode is child node of this
+    neighbors_[toNode->name()] = toNode;
+    // from `this` goto `toNode`, will take `overhead` seconds
+    toNode->overhead_[name()] = overhead;
   }
 
   Neighbors &neighbors() { return neighbors_; }
   NodeName name() const { return name_; }
-  TimeSecond overhead() const { return overhead_; }
+  TimeSecond overhead(const NodeName &name) { return overhead_[name]; }
 
-  Node &getNodeByName(const NodeName &name) {
-    return findNodeByName(*this, name);
+  NodePtr getNodeByName(const NodeName &name) {
+    return findNodeByName(shared_from_this(), name);
   }
 
 private:
-  Node &findNodeByName(Node &node, const NodeName &name) {
-    if (node.name() == name) {
+  NodePtr findNodeByName(const NodePtr &node, const NodeName &name) {
+    if (node->name() == name) {
       return node;
     }
-    for (auto &neighbor : node.neighbors()) {
-      if (neighbor.second.name() == name) {
+    for (auto &neighbor : node->neighbors()) {
+      if (neighbor.second->name() == name) {
         return neighbor.second;
       }
     }
-    for (auto &neighbor : node.neighbors()) {
+    for (auto &neighbor : node->neighbors()) {
       return findNodeByName(neighbor.second, name);
     }
+    return nullptr;
   }
 
   NodeName name_;
   Neighbors neighbors_;
-  TimeSecond overhead_{maxTimeSecond};
+  //`parent node` go to `this`, will table `overhead_[parent]` seconds
+  NodeToNodeTimeSecondMap overhead_;
 };
 
-class Graph : public Node {
+class Graph {
 public:
-  Graph(const NodeName &rootName) : Node(rootName, maxTimeSecond) {}
+  Graph(const NodeName &rootName)
+      : rootNode_(std::make_shared<Node>(rootName)) {}
+  void gotoNode(const NodeName &fromName, const NodeName &toName,
+                TimeSecond overhead) {
+    auto fromNode = getNodeByName(fromName);
+    auto toNode = getNodeByName(toName);
+    if (!toNode) {
+      toNode = std::make_shared<Node>(toName);
+    }
+    fromNode->connect(toNode, overhead);
+  }
+
+  NodePtr getNodeByName(const NodeName &name) {
+    return rootNode_->getNodeByName(name);
+  }
+
   virtual ~Graph(){};
+
+private:
+  friend class GraphPath;
+  NodePtr rootNode_;
 };
+using GraphPtr = std::shared_ptr<Graph>;
 
 class GraphPath {
 public:
-  GraphPath(Graph &graph) { buildCosts(graph, graph.name()); }
+  GraphPath(const GraphPtr &graph) {
+    buildCosts(graph->rootNode_, graph->rootNode_->name());
+  }
   virtual ~GraphPath() {}
   TimeSecond overhead(const NodeName &name) { return costs_[name].overhead; }
   std::vector<NodeName> path(const NodeName &name) {
@@ -84,21 +111,21 @@ private:
     NodeName parent;
     bool handled{false};
   };
-  void buildCosts(Node &node, const NodeName &rootName) {
-    auto &neighbors = node.neighbors();
+  void buildCosts(const NodePtr &node, const NodeName &rootName) {
+    auto &neighbors = node->neighbors();
     if (neighbors.empty()) {
       return;
     }
     for (auto neighbor : neighbors) {
       NodeOverhead overhead;
-      overhead.parent = node.name();
+      overhead.parent = node->name();
       if (overhead.parent == rootName) {
-        overhead.overhead = neighbor.second.overhead();
+        overhead.overhead = neighbor.second->overhead(rootName);
       } else {
         overhead.overhead = maxTimeSecond;
       }
-      if (costs_.find(neighbor.second.name()) == costs_.end()) {
-        costs_[neighbor.second.name()] = overhead;
+      if (costs_.find(neighbor.second->name()) == costs_.end()) {
+        costs_[neighbor.second->name()] = overhead;
       }
       buildCosts(neighbor.second, rootName);
     }
@@ -108,6 +135,7 @@ private:
     TimeSecond lowestOverhead = maxTimeSecond;
     lowestNodename.clear();
 
+    // find lowest from costs_ table, if has handled, skip it
     for (auto it : costs_) {
       NodeName name = it.first;
       NodeOverhead nodeOverhead = it.second;
@@ -119,20 +147,23 @@ private:
     return !lowestNodename.empty();
   }
 
-  friend GraphPath findShortestGraphPath(Graph &graph, const NodeName &end);
+  friend GraphPath findShortestGraphPath(const GraphPtr &graph,
+                                         const NodeName &end);
   std::map<std::string, NodeOverhead> costs_;
 };
 
-inline GraphPath findShortestGraphPath(Graph &graph, const NodeName &end) {
+inline GraphPath findShortestGraphPath(const GraphPtr &graph,
+                                       const NodeName &end) {
   GraphPath p(graph);
 
   NodeName name;
   while (p.getLowestNodeName(name)) {
     TimeSecond overhead = p.costs_[name].overhead;
-    auto node = graph.getNodeByName(name);
-    for (auto neighbor : node.neighbors()) {
-      NodeName neighborName = neighbor.second.name();
-      TimeSecond newOverhead = overhead + neighbor.second.overhead();
+    auto node = graph->getNodeByName(name);
+    for (auto neighbor : node->neighbors()) {
+      NodeName neighborName = neighbor.second->name();
+      TimeSecond newOverhead =
+          overhead + neighbor.second->overhead(node->name());
       if (p.costs_[neighborName].overhead > newOverhead) {
         p.costs_[neighborName].overhead = newOverhead;
         p.costs_[neighborName].parent = name;
--- test/DijkstraTest.cpp
+++ test/DijkstraTest.cpp
@@ -2,13 +2,55 @@
 #include <gtest/gtest.h>
 
 TEST(DijkstraTest, findShortestGraphPath) {
-  algorithm::Graph graph("Start");
-  graph.addNeighbor("A", 6).addNeighbor("End", 1);
-  auto &b = graph.addNeighbor("B", 2);
-  b.addNeighbor("A", 3);
-  b.addNeighbor("End", 5);
+  algorithm::GraphPtr graph = std::make_shared<algorithm::Graph>("Start");
+  graph->gotoNode("Start", "A", 6);
+  graph->gotoNode("Start", "B", 2);
+  graph->gotoNode("A", "End", 1);
+  graph->gotoNode("B", "End", 5);
+  graph->gotoNode("B", "A", 3);
 
   std::vector<algorithm::NodeName> expectPath = {"Start", "B", "A", "End"};
+
+  auto path = algorithm::findShortestGraphPath(graph, "End");
+  auto actualPath = path.path("End");
+  EXPECT_EQ(std::equal(expectPath.begin(), expectPath.end(), actualPath.begin(),
+                       actualPath.end()),
+            true);
+}
+
+TEST(DijkstraTest, findShortestGraphPath2) {
+  algorithm::GraphPtr graph = std::make_shared<algorithm::Graph>("Start");
+  graph->gotoNode("Start", "A", 5);
+  graph->gotoNode("Start", "B", 0);
+  graph->gotoNode("A", "C", 15);
+  graph->gotoNode("C", "End", 20);
+  graph->gotoNode("A", "D", 20);
+  graph->gotoNode("B", "D", 35);
+  graph->gotoNode("B", "C", 30);
+  graph->gotoNode("D", "End", 10);
+
+  std::vector<algorithm::NodeName> expectPath = {"Start", "A", "D", "End"};
+
+  auto path = algorithm::findShortestGraphPath(graph, "End");
+  auto actualPath = path.path("End");
+  EXPECT_EQ(std::equal(expectPath.begin(), expectPath.end(), actualPath.begin(),
+                       actualPath.end()),
+            true);
+}
+
+
+TEST(DijkstraTest, findShortestGraphPath3) {
+  algorithm::GraphPtr graph = std::make_shared<algorithm::Graph>("Start");
+  graph->gotoNode("Start", "A", 5);
+  graph->gotoNode("Start", "B", 2);
+  graph->gotoNode("A", "C", 4);
+  graph->gotoNode("C", "End", 3);
+  graph->gotoNode("A", "D", 2);
+  graph->gotoNode("B", "D", 7);
+  graph->gotoNode("C", "D", 6);
+  graph->gotoNode("D", "End", 1);
+
+  std::vector<algorithm::NodeName> expectPath = {"Start", "A", "D", "End"};
 
   auto path = algorithm::findShortestGraphPath(graph, "End");
   auto actualPath = path.path("End");
